73592004ab74249fbb023310c58ada97d047c3bd
Hiba-Mohamed <hibamoha987@gmail.com>
2024-04-05 18:56:26 -0230

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,25 @@
 # This is the code for the Robot Obstacle Course
+
+def start():
+
+    robot_ctrl.set_mode(rm_define.robot_mode_free)
+
+    def ScanRoomType():
+    
+
+    def FireRoom():
+    
+    def PersonRoom():
+    
+    def PosionRoom():
+    
+
+    if RoomType == 1:
+        FireRoom()
+
+    elif RoomType == 3:
+        PersonRoom()
+        
+    else:
+        PosionRoom()
+    
